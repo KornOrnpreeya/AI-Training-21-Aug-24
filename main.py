ab3d846caba39f9d29334da813783a7cd565ab4e
KornOrnpreeya <ornpreeya.lengwong@gmail.com>
2024-08-21 14:44:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 # create streamlit app to load iris dataset from seaborn
 import streamlit as st
 import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
 df = sns.load_dataset('iris')
 df
+
+x = df.iloc[:, :-1]
+y = df['species']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+
+st.sidebar.title('Classifiers')
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+if classifier =='KNN'
+  knn = KNeighborsClassifier(n_neighbors=1)
+  knn.fit(x_train, y_train)
+  y_pred = knn.predict(x_test)
+  accuracy_score(y_test, y_pred)
+if classifier =='SVM'
+  svm = SVC()
+  svm.fit(x_train, y_train)
+  y_pred = svm.predict(x_test)
+  accuracy_score(y_test, y_pred)
