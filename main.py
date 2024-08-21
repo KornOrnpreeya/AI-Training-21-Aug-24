1b23decf7538ccb4d4d788aeb7cd0d467bf2f1e3
KornOrnpreeya <ornpreeya.lengwong@gmail.com>
2024-08-21 14:46:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ if classifier =='KNN'
   knn = KNeighborsClassifier(n_neighbors=1)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
 if classifier =='SVM'
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
